60adbd69b52dbaecec601e7cbf153f7748e6dd7e
Eamon <eamon0989@gmail.com>
2020-12-07 22:35:16 +0100

    added more to the list

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function rndInt() {
-    let rndInt = Math.floor(Math.random() * 2);
+    let rndInt = Math.floor(Math.random() * 5);
     console.log(rndInt)
 
 let x = rndInt;
@@ -11,17 +11,43 @@ unicorn.setAttribute("src", "unicorn.jpg");
 const art = document.createElement("img");
 art.setAttribute("src", "artist.gif");    
 
+const wife = document.createElement("img");
+wife.setAttribute("src", "wife.jpg");   
+
+const catmom = document.createElement("img");
+catmom.setAttribute("src", "catmom.jpg");   
+
+const travelpartner = document.createElement("img");
+travelpartner.setAttribute("src", "travelpartner.jpg");   
+
 var msgEl = document.getElementById("displayMsg");
 msgEl.innerHTML = "";
 
-var artEl = document.getElementById("displayArt")
+var artEl = document.getElementById("displayMsg")
 artEl.innerHTML = "";
+
+var wifeEl = document.getElementById("displayMsg")
+wifeEl.innerHTML = "";
+
+var catmomEl = document.getElementById("displayMsg")
+catmomEl.innerHTML = "";
+
+var travelpartnerEl = document.getElementById("displayMsg")
+travelpartnerEl.innerHTML = "";
 
 if (x === 0) {
     msgEl.innerHTML = "the most beautiful unicorn in the world!";
     msgEl.appendChild(unicorn);
     } else if (x === 1) {
-        msgEl.innerHTML = "an awesome artist";
+        msgEl.innerHTML = "an awesome artist!";
         msgEl.appendChild(art);
-    }
-};
+    } else if (x === 2) {
+        wifeEl.innerHTML = "the best wife ever!";
+        wifeEl.appendChild(wife);
+    } else if (x === 3) {
+        catmomEl.innerHTML = "the best cat mom ever!";
+        catmomEl.appendChild(catmom);
+    } else if (x === 4) {
+        travelpartnerEl.innerHTML = "the best travel partner I could ever wish for!";
+        travelpartnerEl.appendChild(travelpartner);
+}};
